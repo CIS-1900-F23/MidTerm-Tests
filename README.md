7178111e09397038e6820d01526f171b220a8882
Mohamed Abaker <mabaker@Mohameds-MacBook-Pro.local>
2023-11-05 18:18:12 -0500

    tests

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# MidTerm-Tests
-Tests for midterm project
+#  Midterm-Tests
+
+Before running the tests, do the following:
+
+ - Add any necessary includes to run your pointers.
+ - Since I don't know
+   the names of your classes, I created the aliases at the beginning of
+   the test.cpp file. Currently, the aliases use the std implementation
+   of the smart pointers (which will pass all tests), you will need to
+   rename it to use the name of your implementation.
+
+To run this, you can use 
+
+    g++ -std=c++20 ./test.cpp -g -Wall -o test
+and then run 
+
+    ./test
+If this works without segfaulting and passes all the tests. And if Valgrind reports no memory leaks. You can be fairly confident that your solution works.
---
+++ test.cpp
@@ -0,0 +1,238 @@
+
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <type_traits>
+// TODO: write your includes here
+
+
+
+// TODO: replace the aliases with your own implementations 
+// and remove the include for <memory> 
+#include <memory>
+template <typename T>
+using __UniquePtr = std::unique_ptr<T>;
+
+template <typename T>
+using __SharedPtr = std::shared_ptr<T>;
+
+template <typename T>
+using __WeakPtr = std::weak_ptr<T>;
+
+
+struct Foo 
+{
+    int a;
+    int b;
+
+    Foo(): a{5}, b{5} {}
+};
+
+
+// Needs basic constructors, dereference and arrow operator
+TEST_CASE("Pointers work")
+{
+    int* int_ptr1 = new int{10};
+    __SharedPtr<int>  sp1 {int_ptr1};
+
+    int* int_ptr2 = new int{10};
+    __UniquePtr<int>  up1 {int_ptr2};
+    
+    SECTION("Dereferences works")
+    {
+        REQUIRE(*sp1 == 10);  
+        REQUIRE(*up1 == 10);  
+    }
+
+    *sp1 = 20;
+    *up1 = 20;
+
+    SECTION("Can change values")
+    {
+        REQUIRE(*sp1 == 20);  
+        REQUIRE(*up1 == 20);  
+    }
+
+    Foo* foo_ptr1 = new Foo{};
+    __SharedPtr<Foo> sp2 {foo_ptr1};
+
+    Foo* foo_ptr2 = new Foo{};
+    __UniquePtr<Foo> up2 {foo_ptr2};
+
+    SECTION("Arrow operator works")
+    {
+        REQUIRE(sp2->a == 5); 
+        REQUIRE(sp2->b == 5);  
+
+        REQUIRE(up2->a == 5); 
+        REQUIRE(up2->b == 5);  
+    }
+
+    sp2->a = 20;
+    sp2->b = 20;
+    
+    up2->a = 20;
+    up2->b = 20;
+
+    SECTION("Can change values")
+    {
+        REQUIRE(sp2->a == 20); 
+        REQUIRE(sp2->b == 20);   
+
+        REQUIRE(up2->a == 20); 
+        REQUIRE(up2->b == 20); 
+    }
+}
+
+
+// Needs get, release, and reset
+TEST_CASE("Special Functions work")
+{
+    int* int_ptr1 = new int{10};
+    __SharedPtr<int>  sp1 {int_ptr1};
+    int* int_ptr2 = new int{10};
+    __UniquePtr<int>  up1 {int_ptr2};
+    
+    SECTION("get works")
+    {
+        REQUIRE(*(sp1.get()) == 10);  
+        REQUIRE(*(up1.get()) == 10);  
+    }
+
+    SECTION("relase works")
+    {
+        up1.release();
+        REQUIRE(up1.get() == nullptr);  
+    }
+
+
+
+    SECTION("reset works")
+    {
+        int* int_ptr3 = new int{20};
+        sp1.reset(int_ptr3);
+        REQUIRE(*sp1 == 20);
+
+        int* int_ptr4 = new int{20};
+        up1.reset(int_ptr4);
+        REQUIRE(*up1 == 20);
+        
+    }
+}
+
+
+// Needs special constructors
+TEST_CASE("Special constructors")
+{   
+    SECTION("Deleted Constructors")
+    {
+        REQUIRE(std::is_copy_constructible<__SharedPtr<int>>::value == true);
+        REQUIRE(std::is_move_constructible<__SharedPtr<int>>::value == true);  
+        REQUIRE(std::is_copy_assignable<__SharedPtr<int>>::value == true);
+        REQUIRE(std::is_move_assignable<__SharedPtr<int>>::value == true);  
+        REQUIRE(std::is_default_constructible<__SharedPtr<int>>::value == true);  
+
+        REQUIRE(std::is_copy_constructible<__UniquePtr<int>>::value == false);
+        REQUIRE(std::is_move_constructible<__UniquePtr<int>>::value == true);  
+        REQUIRE(std::is_copy_assignable<__UniquePtr<int>>::value == false);
+        REQUIRE(std::is_move_assignable<__UniquePtr<int>>::value == true);  
+        REQUIRE(std::is_default_constructible<__UniquePtr<int>>::value == true);  
+    }
+
+    SECTION("Copy Constructor Shared")
+    {
+        int* int_ptr1 = new int{10};
+        auto *sp1 = new  __SharedPtr<int>{int_ptr1};
+        __SharedPtr<int>  sp2 {*sp1};
+
+        delete sp1;
+        // sp2 should still be valid
+        REQUIRE(*sp2 == 10);  
+    }
+
+    SECTION("Copy Assignment Shared")
+    {
+        int* int_ptr1 = new int{10};
+        auto *sp1 = new  __SharedPtr<int>{int_ptr1};
+        int* int_ptr2 = new int{20};
+        __SharedPtr<int>  sp2 {int_ptr2};
+        sp2 = *sp1;
+
+        delete sp1;
+        // sp2 should still be valid
+        REQUIRE(*sp2 == 10);  
+    }
+
+    SECTION("Move Constructor")
+    {
+        int* int_ptr1 = new int{10};
+        __SharedPtr<int>  sp1 {int_ptr1};
+        __SharedPtr<int>  sp2 {std::move(sp1)};
+
+        int* int_ptr2 = new int{10};
+        __UniquePtr<int>  up1 {int_ptr2};
+        __UniquePtr<int>  up2 {std::move(up1)};
+
+        REQUIRE(*sp2 == 10);  
+        REQUIRE(sp1.get() == nullptr);  
+
+        REQUIRE(*up2 == 10);  
+        REQUIRE(up1.get() == nullptr);  
+    }
+
+    SECTION("Move Assingment")
+    {
+        int* int_ptr1 = new int{10};
+        __SharedPtr<int>  sp1 {int_ptr1};
+        int* int_ptr2 = new int{20};
+        __SharedPtr<int>  sp2 {int_ptr2};
+        sp2 = std::move(sp1);
+
+        int* int_ptr3 = new int{10};
+        __UniquePtr<int>  up1 {int_ptr3};
+        int* int_ptr4 = new int{20};
+        __UniquePtr<int>  up2 {int_ptr4};
+        up2 = std::move(up1);
+
+        REQUIRE(*sp2 == 10);  
+        REQUIRE(sp1.get() == nullptr);  
+
+        REQUIRE(*up2 == 10);  
+        REQUIRE(up1.get() == nullptr);  
+    }
+}
+
+
+TEST_CASE("Weak Ptr")
+{
+    int* int_ptr1 = new int{10};
+    auto *sp1 = new  __SharedPtr<int>{int_ptr1};
+    auto *sp2 = new  __SharedPtr<int>{*sp1};
+    __WeakPtr<int> wp1 {*sp1};
+    
+    SECTION("expired works")
+    {
+        
+        REQUIRE(wp1.expired() == false);  
+
+        delete sp1;
+        REQUIRE(wp1.expired() == false); 
+
+        delete sp2;
+        REQUIRE(wp1.expired() == true); 
+    }
+
+    SECTION("lock works")
+    {
+        auto sp3 = wp1.lock();
+        
+        REQUIRE(*sp3 == 10); 
+
+        delete sp1;
+        delete sp2;
+
+        // still valid
+        REQUIRE(*sp3 == 10); 
+        REQUIRE(wp1.expired() == false); 
+    }
+}
